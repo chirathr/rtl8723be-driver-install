8ebe69d7904e77bc9a9be71c067cd76892da6f46
Chirath02 <chirath.02@gmail.com>
2017-04-17 18:49:48 +0530

    script to install wifi drivers

---
+++ rtl8723be-install.sh
@@ -0,0 +1,32 @@
+# install rtlwifi-new-dkms
+
+sudo add-apt-repository -y ppa:hanipouspilot/rtlwifi
+sudo apt-get update
+sudo apt-get -y install rtlwifi-new-dkms linux-firmware
+
+# install additional dependencies
+
+sudo apt-get install linux-headers-$(uname -r) gcc g++ build-essential dkms git
+
+# clone latest drivers
+
+git clone https://github.com/lwfinger/rtlwifi_new/tree/master
+
+# make and install realtek driver
+
+cd rtlwifi_new
+make -j2
+sudo make install
+
+
+# add options, select ant
+
+echo "options rtl8723be ant_sel=2 fwlps=0" | sudo tee /etc/modprobe.d/rtl8723be.conf
+
+sudo modprobe -r rtl8723be
+
+sudo modprobe -v rtl8723be ant_sel=2
+
+echo "Install complete!"
+
+echo " If installation failed try the manual install from : http://blog.chirathr.me/2016/05/12/installing-realtek-rtl8723be-driver-for-ubuntu-16-04/" 
